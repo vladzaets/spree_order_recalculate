7118735cd7003382938eb88ba4e3dc135c8d1087
vladzaets <vladzaets@gmail.com>
2015-06-09 21:02:24 +0700

    We don't need it here

--- lib/spree_order_recalculate/order_recalculate.rb
+++ lib/spree_order_recalculate/order_recalculate.rb
@@ -2,6 +2,5 @@ module OrderRecalculate
   def recalculate_order
     @order = current_order || Order.incomplete.find_or_initialize_by(guest_token: cookies.signed[:guest_token])
     @order.update_with_line_items!
-    associate_user
   end
 end
