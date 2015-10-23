811d6d6ba9cb42b8fb4259f1673504b55e54ed7d
vladzaets <vladzaets@gmail.com>
2015-10-23 20:42:54 +0700

    Fix bug

--- lib/spree_order_recalculate/order_recalculate.rb
+++ lib/spree_order_recalculate/order_recalculate.rb
@@ -1,6 +1,6 @@
 module OrderRecalculate
   def recalculate_order
-    @order = current_order || Order.incomplete.find_or_initialize_by(guest_token: cookies.signed[:guest_token])
+    @order = current_order || Spree::Order.incomplete.find_or_initialize_by(guest_token: cookies.signed[:guest_token])
     @order.update_with_line_items!
   end
 end
